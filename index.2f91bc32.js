bd2254dd6b63f33c13c7f8db9b9c8990134db375
Alla Serhiienko <allasergienko706@gmail.com>
2023-09-19 22:31:28 +0200

    Updates

--- index.2f91bc32.js
+++
@@ -1,3 +0,0 @@
-window.addEventListener("hashchange",()=>{"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")});//# sourceMappingURL=index.2f91bc32.js.map
-
-//# sourceMappingURL=index.2f91bc32.js.map
